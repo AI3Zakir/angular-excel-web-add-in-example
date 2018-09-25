b6f1a7ff2f8c0ee0130a41a4495805b443a575bc
Zakirdin <zakirdin.lamiev@gmail.com>
2018-09-26 00:11:00 +0600

    small excel web add in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+declare const Excel: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web-addin-excel-angular';
+
+  color = 'green';
+  sum = 0;
+
+  onColorRows() {
+    Excel.run((context) => {
+      const range = context.workbook.getSelectedRange();
+      range.format.fill.color = this.color;
+      return context.sync();
+    });
+  }
+
+  onAddRows() {
+    Excel.run((context) => {
+      this.sum = 0;
+      const range = context.workbook.getSelectedRange();
+      range.load('values');
+      return context.sync().then( () => {
+        for (const rows of range.m_values) {
+          for (const value of rows) {
+            this.sum += +value;
+          }
+        }
+      });
+    });
+  }
+
+  private objToString (obj) {
+    let str = '';
+    for (const p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        str += p + '::' + obj[p] + '\n';
+      }
+    }
+    return str;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import {MatButtonModule, MatCardModule, MatDividerModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
